be337bd900f27b0770153387547bc14d9a9cdbf0
Jessica Nell <jnell2@comcast.net>
2016-11-03 12:12:10 -0600

    Day 4: consolidating code

--- code/WebScraping.py
+++ code/WebScraping.py
@@ -145,3 +145,50 @@ def Season_GA_Strength(soup):
         mat = np.vstack((mat, row))
     df = pd.DataFrame(mat,columns = cols)
     return df
+
+def get_data():
+
+    date1 = '2016-10-12' # in format YYYY-MM-DD
+    date2 = '2016-10-31' # in format YYYY-MM-DD
+    season1 = '20162017' # in format YYYYyyyy (ie: 20162017)
+    season2 = '20162017' # in format YYYYyyyy (ie: 20162017)
+    season3 = '20152016' # past season
+    season4 = '20152016' # past season
+
+    Date_TeamSummary_url = 'http://www.nhl.com/stats/team?aggregate=0&gameType=2&report=teamsummary&reportType=game&startDate={}&endDate={}&filter=gamesPlayed,gte,&sort=wins,points'.format(date1, date2)
+    Date_Penalties_url = 'http://www.nhl.com/stats/team?aggregate=0&gameType=2&report=penalties&reportType=game&startDate={}&endDate={}&filter=gamesPlayed,gte,&sort=penaltyMinutes'.format(date1, date2)
+    Date_Shots_url = 'http://www.nhl.com/stats/team?aggregate=0&gameType=2&report=realtime&reportType=game&startDate={}&endDate={}&filter=gamesPlayed,gte,&sort=hits'.format(date1, date2)
+    Season_TeamSummary_url = 'http://www.nhl.com/stats/team?aggregate=0&gameType=2&report=teamsummary&reportType=season&seasonFrom={}&seasonTo={}&filter=gamesPlayed,gte,&sort=points,wins,gamesPlayed'.format(season1, season2)
+    Season_TeamSummary_past_url = 'http://www.nhl.com/stats/team?aggregate=0&gameType=2&report=teamsummary&reportType=season&seasonFrom={}&seasonTo={}&filter=gamesPlayed,gte,&sort=points,wins,gamesPlayed'.format(season3, season4)
+    Season_Shots_url = 'http://www.nhl.com/stats/team?aggregate=0&gameType=2&report=realtime&reportType=season&seasonFrom={}&seasonTo={}&filter=gamesPlayed,gte,&sort=hits'.format(season1, season2)
+    Season_GF_url = 'http://www.nhl.com/stats/team?aggregate=0&gameType=2&report=goalsbystrength&reportType=season&seasonFrom={}&seasonTo={}&filter=gamesPlayed,gte,&sort=goalsFor'.format(season1, season2)
+    Season_GA_url = 'http://www.nhl.com/stats/team?aggregate=0&gameType=2&report=goalsagainstbystrength&reportType=season&seasonFrom={}&seasonTo={}&filter=gamesPlayed,gte,&sort=goalsAgainst'.format(season1, season2)
+
+    soup_dts = get_soup(Date_TeamSummary_url)
+    df_dts = Date_TS(soup_dts)
+
+    soup_dp = get_soup(Date_Penalties_url)
+    df_dp = Date_Penalties(soup_dp)
+
+    soup_ds = get_soup(Date_Shots_url)
+    df_ds = Date_Shots(soup_ds)
+
+    soup_sts = get_soup(Season_TeamSummary_url)
+    df_sts = Season_TS(soup_sts)
+    soup_sts_past = get_soup(Season_TeamSummary_past_url)
+    df_sts_past = Season_TS(soup_sts_past)
+
+    soup_ss = get_soup(Season_Shots_url)
+    df_ss = Season_Shots(soup_ss)
+
+    soup_sgf = get_soup(Season_GF_url)
+    df_sgf = Season_GF_Strength(soup_sgf)
+
+    soup_sga = get_soup(Season_GA_url)
+    df_sga = Season_GA_Strength(soup_sga)
+
+    return df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga
+
+if __name__ == '__main__':
+
+    df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga = get_data()
