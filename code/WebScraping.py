7f11c669855d2a09aac1ddf204dfbc2294b9e258
Jessica Nell <jnell2@comcast.net>
2016-11-04 15:30:15 -0600

    Day 5: Web scraping is finally getting the right things!

--- code/WebScraping.py
+++ code/WebScraping.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # make sure that you have Google Chrome and selenium installed!
 
@@ -17,7 +19,7 @@ def get_soup(url, page):
     # this will launch a new Chrome browser (maybe multiple)
     # don't exit out until process is finished running
     d.get(url)
-    d.find_element(By.XPATH, "//select[@class='pager-select']/option[text()='{}']".format(page))
+    d.find_element(By.XPATH, "//select[@class='pager-select']/option[text()='{}']".format(page)).click()
     result = d.page_source
     soup = BeautifulSoup(result, 'html.parser')
     d.close()
@@ -155,14 +157,19 @@ def Date_TS_loop(url, num_pages):
     will return a dataframe with all of the pages together
     gives us the df for the game-by-game team summary (total)
     '''
+    # creating a dictionary of soup outputs
+    # length should be equal to number of pages in table
     soup_dct = {}
     for i in range(1,num_pages+1):
         soup_dct[i] = get_soup(url, page=i)
+    # creating a dictionary of dataframes
+    # length should be equal to number of pages in table
     df_dct = {}
     for p, soup in soup_dct.items():
         df_dct[p] = Date_TS(soup)
+    # appending all dataframes together
     for i in range(2,num_pages+1):
-        df_dct[1].append(df_dct[i])
+        df_dct[1] = df_dct[1].append(df_dct[i])
     df_dts = df_dct[1]
     return df_dts
     # del soup_dct
@@ -174,14 +181,19 @@ def Date_Penalties_loop(url, num_pages):
     will return a dataframe with all of the pages together
     gives us the df for the game-by-game penalties report (total)
     '''
+    # creating a dictionary of soup outputs
+    # length should be equal to number of pages in table
     soup_dct = {}
     for i in range(1,num_pages+1):
         soup_dct[i] = get_soup(url, page=i)
+    # creating a dictionary of dataframes
+    # length should be equal to number of pages in table
     df_dct = {}
     for p, soup in soup_dct.items():
         df_dct[p] = Date_Penalties(soup)
+    # appending all dataframes together
     for i in range(2,num_pages+1):
-        df_dct[1].append(df_dct[i])
+        df_dct[1] = df_dct[1].append(df_dct[i])
     df_dp = df_dct[1]
     return df_dp
     # del soup_dct
@@ -193,14 +205,19 @@ def Date_Shots_loop(url, num_pages):
     will return a dataframe with all of the pages together
     gives us the df for the game-by-game shots report (total)
     '''
+    # creating a dictionary of soup outputs
+    # length should be equal to number of pages in table
     soup_dct = {}
     for i in range(1,num_pages+1):
         soup_dct[i] = get_soup(url, page=i)
+    # creating a dictionary of dataframes
+    # length should be equal to number of pages in table
     df_dct = {}
     for p, soup in soup_dct.items():
         df_dct[p] = Date_Shots(soup)
+    # appending all dataframes together
     for i in range(2,num_pages+1):
-        df_dct[1].append(df_dct[i])
+        df_dct[1] = df_dct[1].append(df_dct[i])
     df_ds = df_dct[1]
     return df_ds
     # del soup_dct
@@ -226,19 +243,6 @@ def get_data():
 
     num_pages = 7 # change number of pages, get from url for any of the game-by-game tables
     # this number will be the same for all game-by-game tables (3 in total)
-
-    # soup_dts = {}
-    # num_pages = 6 # change number of pages, get from url for this table
-    # for i in range(1,num_pages+1):
-    #     soup_dts[i] = get_soup(Date_TeamSummary_url, page=i)
-    # df_dct = {}
-    # for p, soup in soup_dts:
-    #     df_dct[p] = Date_TS(soup)
-    # for i in range(2,num_pages+1):
-    #     df_dct[1].append(df_dct[i])
-    # df_dts = df_dct[1]
-    # del soup_dts
-    # del df_dct
 
     df_dts = Date_TS_loop(Date_TeamSummary_url, num_pages)
     df_dp = Date_Penalties_loop(Date_Penalties_url, num_pages)
