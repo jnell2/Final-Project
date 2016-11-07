79dba21a9909fa03c2349830b6c711a3478c7df8
Jessica Nell <jnell2@comcast.net>
2016-11-06 21:30:47 -0700

    data cleaning, making 4 tables

--- code/DataCleaning.py
+++ code/DataCleaning.py
@@ -74,15 +74,8 @@ def clean_dp(df):
     df['date'] = df['game'].str.split().str[0]
     df['PIM'] = df['PIM'].astype(int)
     df['penalties'] = df['penalties'].astype(int)
-    df.drop(['#', 'game', 'GP', 'W', 'L', 'T', 'OTL', 'points', 'minor', 'major', 'misconduct', 'game_misconduct', 'match'], axis=1, inplace=True)
-    df = df.reset_index(drop=True)
     df['date'] = pd.to_datetime(df['date'])
-    idx = df.groupby('team')['date'].nsmallest(10).index.get_level_values(1)
-    df['last10'] = np.where(df.index.isin(idx),1,0)
-    idx2 = df.groupby('team')['date'].nsmallest(5).index.get_level_values(1)
-    df['last5'] = np.where(df.index.isin(idx2),1,0)
-    idx3 = df.groupby('team')['date'].nsmallest(2).index.get_level_values(1)
-    df['last2'] = np.where(df.index.isin(idx3),1,0)
+    df.drop(['#', 'game', 'GP', 'W', 'L', 'T', 'OTL', 'points', 'minor', 'major', 'misconduct', 'game_misconduct', 'match'], axis=1, inplace=True)
     return df
 
 def clean_ds(df):
@@ -98,15 +91,8 @@ def clean_ds(df):
     df['giveaways'] = df['giveaways'].astype(int)
     df['takeaways'] = df['takeaways'].astype(int)
     df['save%'] = df['save%'].astype(float)
-    df.drop(['#', 'game', 'GP', 'W', 'L', 'T', 'OTL', 'points', 'FOW', 'FOL', 'FO', 'FOW%', 'SF', 'GF'], axis=1, inplace=True)
-    df = df.reset_index(drop=True)
     df['date'] = pd.to_datetime(df['date'])
-    idx = df.groupby('team')['date'].nsmallest(10).index.get_level_values(1)
-    df['last10'] = np.where(df.index.isin(idx),1,0)
-    idx2 = df.groupby('team')['date'].nsmallest(5).index.get_level_values(1)
-    df['last5'] = np.where(df.index.isin(idx2),1,0)
-    idx3 = df.groupby('team')['date'].nsmallest(2).index.get_level_values(1)
-    df['last2'] = np.where(df.index.isin(idx3),1,0)
+    df.drop(['#', 'game', 'GP', 'W', 'L', 'T', 'OTL', 'points', 'FOW', 'FOL', 'FO', 'FOW%', 'SF', 'GF'], axis=1, inplace=True)
     return df
 
 def clean_sts(df, df2):
@@ -132,7 +118,7 @@ def clean_sts(df, df2):
     df['SA/GP'] = df['SA/GP'].astype(float)
     df['FOW%'] = df['FOW%'].astype(float)
     df['SA'] = df['SA/GP'] * df['GP'] # calculated approximate SA
-    df.drop(['#', 'season', 'T', 'OTL', 'points', 'ROW'], axis=1, inplace=True)
+    df.drop(['season', 'T', 'OTL', 'points', 'ROW'], axis=1, inplace=True)
     df2['team'] = df2['team'].str.split().str[-1]
     df2.replace({'team': abbr_dict}, inplace = True)
     df2 = df2[['rank', 'team']]
@@ -151,6 +137,7 @@ def clean_ss(df):
     df['missed_shots'] = df['missed_shots'].astype(int)
     df['giveaways'] = df['giveaways'].astype(int)
     df['takeaways'] = df['takeaways'].astype(int)
+    df['GF'] = df['GF'].astype(int)
     df['SF'] = df['SF'].astype(int)
     df['save%'] = df['save%'].astype(float)
     df['shot%'] = df['GF']/df['SF'] # calculated shot%
@@ -184,9 +171,11 @@ def clean_sga(df):
     df = df[['team', 'GA55', 'GA54', 'GA45']]
     return df
 
-def get_clean_data():
-
-    df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga = ws.get_data()
+def get_clean_data(df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga):
+    '''
+    pass in the 8 dataframes, in the order listed above
+    will return the cleaned up dataframes
+    '''
     df_dts = clean_dts(df_dts)
     df_dp = clean_dp(df_dp)
     df_ds = clean_ds(df_ds)
@@ -196,7 +185,39 @@ def get_clean_data():
     df_sga = clean_sga(df_sga)
     return df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga
 
+def make_GbG_dfs(df_dts, df_dp, df_ds):
+    '''
+    pass in the three appropriate dataframes containing game-by-game data
+    will return a three dataframes with all relevant information for a GbG basis
+    returned dataframes will contain games for past 10, 5, and 2 games, respectively
+    '''
+    df = pd.merge(df_dts, df_dp, how = 'left', on = ['team', 'opponent', 'date'])
+    df = pd.merge(df, df_ds, how = 'left', on ['team', 'opponent', 'date'])
+    df['spread'] = df['GF']-df['GA']
+    df_last10 = df.loc[df['last10'] == 1]
+    df_last5 = df.loc[df['last5'] == 1]
+    df_last2 = df.loc[df['last2'] == 1]
+    return df_last10, df_last5, df_last2
+
+def make_season_df(df_sts, df_sts_past, df_ss, df_sgf, df_sga):
+    '''
+    pass in the 5 appropriate dataframes containing season data
+    will return a single dataframe containing all season data
+    '''
+    df = pd.merge(df_sts, df_sts_past, how = 'left', on = ['team'])
+    df = pd.merge(df, df_ss, how = 'left', on = ['team'])
+    df = pd.merge(df, df_sgf, how = 'left', on = ['team'])
+    df = pd.merge(df, df_sga, how = 'left', on = ['team'])
+    df['corsi'] = df['SF'] - df['SA']
+    return df
 
 if __name__ == '__main__':
 
-    df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga = get_clean_data()
+    df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga = ws.get_data()
+
+    df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga = \
+    get_clean_data(df_dts, df_dp, df_ds, df_sts, df_sts_past, df_ss, df_sgf, df_sga)
+
+    df_last10, df_last5, df_last2 = make_GbG_dfs(df_dts, df_dp, df_ds)
+    df_season = make_season_df(df_sts, df_sts_past, df_ss, df_sgf, df_sga)
+    
